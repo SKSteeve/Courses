7ffc98b25763ca70b3a15c969946ba9d698f33ed
SKSteeve <steeve1997.ss@gmail.com>
2019-12-24 20:24:29 +0200

    Edited

--- 02.TechFunfamentals-php/02.ExercisesTech/23.String-and-Text_Processing-Exercise-php/01.ValidUsernames.php
+++ 02.TechFunfamentals-php/02.ExercisesTech/23.String-and-Text_Processing-Exercise-php/01.ValidUsernames.php
@@ -1,1 +1,25 @@
 <?php
+
+$userNames = explode(', ', readline());
+
+foreach ($userNames as $username)
+{
+    $length = strlen($username);
+    if($length >= 3 && $length <= 16)
+    {
+        $isValid = true;
+        for($i = 0; $i < $length; $i++)
+        {
+            $currentChar = $username[$i];
+            if(!(ctype_alnum($currentChar) || $currentChar === "_" || $currentChar === "-"))
+            {
+                $isValid = false;
+                break;
+            }
+        }
+        if($isValid)
+        {
+            echo $username.PHP_EOL;
+        }
+    }
+}
