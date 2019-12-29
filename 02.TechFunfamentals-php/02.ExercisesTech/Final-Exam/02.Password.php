f84a3c20b0ef2f46bdd3a2672d64ac55aee9cf53
SKSteeve <steeve1997.ss@gmail.com>
2019-12-29 22:44:59 +0200

    ex 2 done

--- 02.TechFunfamentals-php/02.ExercisesTech/Final-Exam/02.Password.php
+++ 02.TechFunfamentals-php/02.ExercisesTech/Final-Exam/02.Password.php
@@ -1,2 +1,34 @@
 <?php
 
+$n = intval(readline());
+
+for ($i = 0; $i < $n; $i++)
+{
+    $password = readline();
+    $firstSymbol = strpos($password, '>');
+    $lastSymbol = strrpos($password, '<');
+
+    $firstGroup = substr($password, 0, $firstSymbol);
+    $lastGroup = substr($password, $lastSymbol + 1);
+
+    if ($firstGroup === $lastGroup)
+    {
+        $password = substr_replace($password, '', $lastSymbol, strlen($password));
+        $password = substr_replace($password, '', 0, $firstSymbol + 1);
+        $pattern = '/[\d]{3}\|[a-z]{3}\|[A-Z]{3}\|[^<>]{3}/';
+
+        if(preg_match($pattern, $password))
+        {
+            echo "Password: " . str_replace('|', '', $password) . PHP_EOL;
+        }
+        else
+        {
+            echo "Try another password!" . PHP_EOL;
+        }
+
+    }
+    else
+    {
+        echo "Try another password!" . PHP_EOL;
+    }
+}
--- 02.TechFunfamentals-php/02.ExercisesTech/Final-Exam/03.Followers.php
+++ 02.TechFunfamentals-php/02.ExercisesTech/Final-Exam/03.Followers.php
@@ -1,1 +1,6 @@
 <?php
+
+$password = readline();
+$lastGroup = strrpos($password, '<');
+
+echo substr($password, $lastGroup + 1) . PHP_EOL;
