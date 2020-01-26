c9fbf7d4095270a3f9121240d5c7d6ec0e636446
SKSteeve <steeve1997.ss@gmail.com>
2020-01-26 23:05:09 +0200

    Done

--- 03.PHP-Web-Module-php/01.PHP-Web-Basics/09.Defining-Classes-Exercise/08.DNAHelix.php
+++ 03.PHP-Web-Module-php/01.PHP-Web-Basics/09.Defining-Classes-Exercise/08.DNAHelix.php
@@ -1,3 +1,33 @@
 <?php
 
 $input = intval(readline());
+dnaHelix($input);
+
+function dnaHelix($number)
+{
+    $str = "ATCGTTAGGG";
+    $counter = 0;
+
+    for($i = 0; $i < $number; $i++)
+    {
+        if($i % 4 == 0)
+        {
+            echo "**" . $str[$counter % 10] . $str[$counter % 10 + 1] . "**";
+        }
+        else if($i % 4 == 1)
+        {
+            echo "*" . $str[$counter % 10] . "--" . $str[$counter % 10 + 1] . "*";
+        }
+        else if($i % 4 == 2)
+        {
+            echo $str[$counter % 10] . "----" . $str[$counter % 10 + 1];
+        }
+        else if($i % 4 == 3)
+        {
+            echo "*" . $str[$counter % 10] . "--" . $str[$counter % 10 + 1] . "*";
+        }
+
+        $counter += 2;
+        echo PHP_EOL;
+    }
+}
--- 03.PHP-Web-Module-php/01.PHP-Web-Basics/09.Defining-Classes-Exercise/09.LastDigitName.php
+++ 03.PHP-Web-Module-php/01.PHP-Web-Basics/09.Defining-Classes-Exercise/09.LastDigitName.php
@@ -1,1 +1,74 @@
 <?php
+
+class Number
+{
+    /**
+     * @var int
+     */
+    private $number;
+
+    public function __construct($number)
+    {
+        $this->setNumber($number);
+    }
+
+    /**
+     * @param int $number
+     */
+    private function setNumber(int $number): void
+    {
+        $this->number = $number;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNumber(): string
+    {
+        if($this->number % 10 == 1)
+        {
+            return "one";
+        }
+        else if($this->number % 10 == 2)
+        {
+            return "two";
+        }
+        else if($this->number % 10 == 3)
+        {
+            return "three";
+        }
+        else if($this->number % 10 == 4)
+        {
+            return "four";
+        }
+        else if($this->number % 10 == 5)
+        {
+            return "five";
+        }
+        else if($this->number % 10 == 6)
+        {
+            return "six";
+        }
+        else if($this->number % 10 == 7)
+        {
+            return "seven";
+        }
+        else if($this->number % 10 == 8)
+        {
+            return "eight";
+        }
+        else if($this->number % 10 == 9)
+        {
+            return "nine";
+        }
+        else if($this->number % 10 == 0)
+        {
+            return "zero";
+        }
+    }
+}
+
+$n = readline();
+$newN = new Number($n);
+
+echo $newN->getNumber();
