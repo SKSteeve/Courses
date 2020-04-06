d8c13401d8a10fe5f8db2fe9ac3a95fc293d3801
SKSteeve <steeve1997.ss@gmail.com>
2020-04-06 17:00:24 +0300

    Done

---
+++ 05.MySQL-DB/12.Subqueries-and-JOINs-Exercise/08.Employee24.sql
@@ -0,0 +1,21 @@
+
+USE `soft_uni`;
+
+SELECT
+	e.`employee_id`,
+	e.`first_name`,
+	IF(YEAR(p.start_date) >= 2005, NULL, p.name) AS 'project_name'
+FROM
+	`employees` AS e
+	JOIN
+		`employees_projects` AS ep
+		ON e.`employee_id` = ep.`employee_id`
+	JOIN
+		`projects` AS p
+		ON ep.`project_id` = p.`project_id`
+WHERE
+	e.employee_id = 24
+ORDER BY
+	e.`first_name` ASC,
+	p.`name` ASC
+LIMIT 5;
