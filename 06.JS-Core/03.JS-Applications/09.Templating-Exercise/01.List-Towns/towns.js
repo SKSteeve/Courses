4c5ee34056b428ac591e574a80cfcf52f6026338
SKSteeve <steeve1997.ss@gmail.com>
2020-10-27 18:52:45 +0200

    Done

---
+++ 06.JS-Core/03.JS-Applications/09.Templating-Exercise/01.List-Towns/towns.js
@@ -0,0 +1,18 @@
+function attachEvents() {
+
+    $('#btnLoadTowns').on('click', getTowns);
+
+    function getTowns() {
+        let towns = $('#towns').val().split(', ');
+        //get template
+        let template = $('#towns-template').html();
+        //compile template
+        let compiledTemplate = Handlebars.compile(template);
+        //create context
+        let context = {
+            towns: towns
+        };
+        //add to html
+        $('#root').html(compiledTemplate(context));
+    }
+}
